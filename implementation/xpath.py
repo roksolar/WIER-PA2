f0c2347d48a3b64aecdc2b085f572082afdb290c
Blaz Marolt <blaz.marolt.isystem@gmail.com>
2019-04-30 13:33:43 +0200

    Added partis.si

--- implementation/xpath.py
+++ implementation/xpath.py
@@ -5,110 +5,172 @@ import json
 from bs4 import BeautifulSoup
 
 
-# We rather use the locally-cached file as it may have changed online.
-pageContent = open('../input/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html', 'r').read()
-#print("Page content:\n'%s'." % pageContent)
+def rtvslo(file):
+    # We rather use the locally-cached file as it may have changed online.
+    pageContent = open(file, 'r').read()
+    #print("Page content:\n'%s'." % pageContent)
 
-dataItem = {
-        "Title": "",
-        "Subtitle": "",
-        "Author": "",
-        "Time": "",
-        "Lead": "",
-        "Content": ""
-    }
+    dataItem = {
+            "Title": "",
+            "Subtitle": "",
+            "Author": "",
+            "Time": "",
+            "Lead": "",
+            "Content": ""
+        }
 
-# Form an XML tree using lxml library
-tree = html.fromstring(pageContent)
-title = str(tree.xpath('//header[@class="article-header"]/h1/text()'))
-dataItem["Title"] = title
+    # Form an XML tree using lxml library
+    tree = html.fromstring(pageContent)
+    title = str(tree.xpath('//header[@class="article-header"]/h1/text()'))
+    dataItem["Title"] = title
 
-subtitle = str(tree.xpath('//div[@class="subtitle"]/text()'))
-dataItem["Subtitle"] = subtitle
+    subtitle = str(tree.xpath('//div[@class="subtitle"]/text()'))
+    dataItem["Subtitle"] = subtitle
 
-author = str(tree.xpath('//div[@class="author-timestamp"]/strong/text()'))
-dataItem["Author"] = author
+    author = str(tree.xpath('//div[@class="author-timestamp"]/strong/text()'))
+    dataItem["Author"] = author
 
-time = str(tree.xpath('//div[@class="author-timestamp"]/text()[2]'))
-dataItem["Time"] = time
+    time = str(tree.xpath('//div[@class="author-timestamp"]/text()[2]'))
+    dataItem["Time"] = time
 
-lead = str(tree.xpath('//p[@class="lead"]/text()'))
-dataItem["Lead"] = lead
+    lead = str(tree.xpath('//p[@class="lead"]/text()'))
+    dataItem["Lead"] = lead
 
-content = str(tree.xpath(' .//article[@class="article"]/p//text()'))
-dataItem["Content"] = content
+    content = str(tree.xpath(' .//article[@class="article"]/p//text()'))
+    dataItem["Content"] = content
 
-print("Output object:\n%s" % json.dumps(dataItem, indent=4, ensure_ascii=False))
+    print("Output object:\n%s" % json.dumps(dataItem, indent=4, ensure_ascii=False))
 
 #OVERSTOCK WEBSITE
-pageContent = open('../input/overstock.com/jewelry02.html', 'r').read()
-#print("Page content:\n'%s'." % pageContent)
+def overstock(file):
+    pageContent = open(file, 'r').read()
+    #print("Page content:\n'%s'." % pageContent)
 
-# Form an XML tree using lxml library
-tree = html.fromstring(pageContent)
+    # Form an XML tree using lxml library
+    tree = html.fromstring(pageContent)
 
-title = str(tree.xpath('.//table[@cellpadding="2"]/tbody/tr/td[2]/a/b/text()'))
-extracted = []
+    title = str(tree.xpath('.//table[@cellpadding="2"]/tbody/tr/td[2]/a/b/text()'))
+    extracted = []
 
-regex = r"'(.*?)'|\"(.*?)\""
-
-
-matches = re.finditer(regex, title)
-
-for tit in matches:
-    dataItem = {
-        "Title": "",
-        "Content": "",
-        "ListPrice": "",
-        "Price": "",
-        "Saving": "",
-        "SavingPercent": ""
-    }
-    dataItem["Title"] = tit.group(1)
-    extracted.append(dataItem)
-
-listPrice = str(tree.xpath('.//table[@cellpadding="2"]/tbody/tr/td[2]/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/s/text()'))
-i = 0
-
-regex = r"'(\$.*?)'"
-matches = re.finditer(regex, listPrice)
-for tit in matches:
-    extracted[i]["ListPrice"] = tit.group(1)
-    i = i + 1
-
-price = str(tree.xpath('.//table[@cellpadding="2"]/tbody/tr/td[2]/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/span/b/text()'))
+    regex = r"'(.*?)'|\"(.*?)\""
 
 
-i = 0
+    matches = re.finditer(regex, title)
 
-regex = r"'(\$.*?)'"
-matches = re.finditer(regex, price)
-for tit in matches:
-    extracted[i]["Price"] = tit.group(1)
-    i = i + 1
+    for tit in matches:
+        dataItem = {
+            "Title": "",
+            "Content": "",
+            "ListPrice": "",
+            "Price": "",
+            "Saving": "",
+            "SavingPercent": ""
+        }
+        dataItem["Title"] = tit.group(1)
+        extracted.append(dataItem)
+
+    listPrice = str(tree.xpath('.//table[@cellpadding="2"]/tbody/tr/td[2]/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/s/text()'))
+    i = 0
+
+    regex = r"'(\$.*?)'"
+    matches = re.finditer(regex, listPrice)
+    for tit in matches:
+        extracted[i]["ListPrice"] = tit.group(1)
+        i = i + 1
+
+    price = str(tree.xpath('.//table[@cellpadding="2"]/tbody/tr/td[2]/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/span/b/text()'))
 
 
-saving = str(tree.xpath('.//table[@cellpadding="2"]/tbody/tr/td[2]/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/span/text()'))
+    i = 0
 
-regex = r"(\$.*?) \((.*?\d{1,3}%)\)"
+    regex = r"'(\$.*?)'"
+    matches = re.finditer(regex, price)
+    for tit in matches:
+        extracted[i]["Price"] = tit.group(1)
+        i = i + 1
 
-matches = re.finditer(regex, saving)
 
-i = 0
-for match in matches:
-    extracted[i]["Saving"] = match.group(1)
-    extracted[i]["SavingPercent"] = match.group(2)
-    i += 1
+    saving = str(tree.xpath('.//table[@cellpadding="2"]/tbody/tr/td[2]/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/span/text()'))
 
-content = str(tree.xpath('.//table[@cellpadding="2"]/tbody/tr/td[2]/table/tbody/tr/td[2]/span/text()'))
+    regex = r"(\$.*?) \((.*?\d{1,3}%)\)"
 
-regex = r"'(.*?)'|\"(.*?)\""
+    matches = re.finditer(regex, saving)
 
-matches = re.finditer(regex, content)
+    i = 0
+    for match in matches:
+        extracted[i]["Saving"] = match.group(1)
+        extracted[i]["SavingPercent"] = match.group(2)
+        i += 1
 
-i = 0
-for tit in matches:
-    extracted[i]["Content"] = tit.group(1)
-    i = i + 1
+    content = str(tree.xpath('.//table[@cellpadding="2"]/tbody/tr/td[2]/table/tbody/tr/td[2]/span/text()'))
 
-#print("Output object:\n%s" % json.dumps(extracted, indent=4, ensure_ascii=False))
+    regex = r"'(.*?)'|\"(.*?)\""
+
+    matches = re.finditer(regex, content)
+
+    i = 0
+    for tit in matches:
+        extracted[i]["Content"] = tit.group(1)
+        i = i + 1
+
+    #print("Output object:\n%s" % json.dumps(extracted, indent=4, ensure_ascii=False))
+
+def partis(file):
+    pageContent = open(file, 'r').read()
+    #print("Page content:\n'%s'." % pageContent)
+    extracted = []
+    # Form an XML tree using lxml library
+    tree = html.fromstring(pageContent)
+
+    naslov = str(tree.xpath('.//div[@class="list"]/div/div[2]/a/text()'))
+
+    matches = naslov.split("',")
+
+    for tit in matches:
+        dataItem = {
+            "Naslov": "",
+            "Podnaslov": "",
+            "Velikost": "",
+            "Sejalcev": "",
+            "Pijavk": "",
+            "Prenosov": ""
+        }
+        dataItem["Naslov"] = replace_all(tit)
+        extracted.append(dataItem)
+
+    podnaslov = str(tree.xpath('.//div[@class="list"]/div/div[2]/div/div[1]/text()'))
+    velikost = str(tree.xpath('.//div[@class="list"]/div/div[4]/text()'))
+    sejalcev = str(tree.xpath('.//div[@class="list"]/div/div[5]/text()'))
+    pijavk = str(tree.xpath('.//div[@class="list"]/div/div[6]/text()'))
+    prenosov = str(tree.xpath('.//div[@class="list"]/div/div[7]/text()'))
+
+    velikost = velikost.split("',")
+    podnaslov = podnaslov.split("',")
+    sejalcev = sejalcev.split("',")
+    pijavk = pijavk.split("',")
+    prenosov = prenosov.split("',")
+
+    i = 0
+    for match in velikost:
+        extracted[i]["Podnaslov"] = replace_all(podnaslov[i])
+        extracted[i]["Velikost"] = replace_all(velikost[i])
+        extracted[i]["Sejalcev"] = replace_all(sejalcev[i])
+        extracted[i]["Pijavk"] = replace_all(pijavk[i])
+        extracted[i]["Prenosov"] = replace_all(prenosov[i])
+        i += 1
+
+    print("Output object:\n%s" % json.dumps(extracted, indent=4, ensure_ascii=False))
+
+def replace_all(besedilo):
+    return besedilo.replace(']','').replace('[','').replace('"','').replace("'",'').lstrip()
+
+overstock_file_1 = '../input/overstock.com/jewelry01.html'
+overstock_file_2 = '../input/overstock.com/jewelry02.html'
+
+rtvslo_file_1 = '../input/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html'
+rtvslo_file_2 = '../input/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html'
+
+partis_file_1 = '../input/partis.si/Partis.si.html'
+partis_file_2 = '../input/partis.si/Partis2.si.html'
+
+partis(partis_file_2)
