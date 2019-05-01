65851336821e875712cf83d75cdc9aebd9ba3997
Anze Versnik <anze.versnik@cosylab.com>
2019-05-01 22:37:01 +0200

    Added matching algorithm

--- implementation/road_runner.py
+++ implementation/road_runner.py
@@ -1,11 +1,15 @@
 from lxml import etree
 from html.parser import HTMLParser
+import collections
 import io
 
 class MyHTMLParser1(HTMLParser):
     def handle_starttag(self, tag, attrs):
         # TODO: kaj z atributi?
-        wrapper.append("<"+ tag+ ">")
+        if len(attrs) == 0:
+            wrapper.append("<"+ tag+ ">")
+        else:
+            wrapper.append("<" + tag + str(attrs).strip('[]()') + ">")
         #print("Encountered a start tag: <"+ tag+ ">")
 
     def handle_endtag(self, tag):
@@ -19,24 +23,41 @@ class MyHTMLParser1(HTMLParser):
 class MyHTMLParser2(HTMLParser):
     def handle_starttag(self, tag, attrs):
         #TODO: kaj z atributi?
-        list_of_tokens.append("<"+ tag+ ">")
+        if len(attrs) == 0:
+            sample.append("<"+ tag+ ">")
+        else:
+            sample.append("<" + tag + str(attrs).strip('[]()').replace("'", "") + ">")
         #print("Encountered a start tag: <"+ tag+ ">")
 
     def handle_endtag(self, tag):
-        list_of_tokens.append("<" + tag + "/>")
+        sample.append("<" + tag + "/>")
         #print("Encountered an end tag : </"+ tag+ ">")
 
     def handle_data(self, data):
-        list_of_tokens.append(data)
+        sample.append(data)
         #print("Encountered some data  :"+ data)
 
 
 class MatchingMethods:
-    def string_mismatches(self, w_string, s_string):
-        if w_string == s_string:
-            return "#PCDATA"
+
+    def both_string(self, w_ele, s_ele):
+        return "<" not in w_ele and "<" not in s_ele
+
+    def get_tag_mismatch(self, count, s_list):
+        Mismatch = collections.namedtuple('Point', ['string', 'counter'])
+        optional = ""
+        start_mis = s_list[count]
+        end_mis = start_mis.replace(">", "/>")
+        if "/>" in s_list[count]:
+            return Mismatch(string="( "+ start_mis +" )?" , counter=1)
         else:
-            return w_string
+            counter = 0
+            while True:
+                optional += s_list[count] +" "
+                counter += 1
+                if end_mis == s_list[count]:
+                    return Mismatch(string="( "+ optional +" )?", counter=counter)
+
     def find_square(self, w_list, s_list):
         ul_w = [i for i, x in enumerate(w_list) if x == "<ul>"]
         ul_s = [i for i, x in enumerate(s_list) if x == "</ul>"]
@@ -71,14 +92,37 @@ parser = etree.HTMLParser()
 tree = etree.parse(io.StringIO(data), parser)
 result = etree.tostring(tree.getroot(), pretty_print=True, method="html") #xhtml format
 
-list_of_tokens = []
+sample = []
 # Generate list of tokens for page 2
 parser = MyHTMLParser2()
 parser.feed(result.decode("utf-8").replace('\n', '').replace('\t', '').replace('\r', ''))
 
-print(list_of_tokens)
+print(sample)
 #-------------------------------------------
 
 # TODO: generate wrapper
 
+wrapper_count = 0
+sample_count = 0
+generated_wrapper = []
+matching = MatchingMethods()
 
+for i in range(max(len(sample), len(wrapper))):
+    sample_ele = sample[sample_count]
+    wrapper_ele = wrapper[wrapper_count]
+    if sample_ele == wrapper_ele:
+        generated_wrapper.append(sample_ele)
+        wrapper_count += 1
+        sample_count += 1
+        continue
+    else:
+        if matching.both_string(sample_ele, wrapper_ele): #string mismatch
+            generated_wrapper.append("#PCDATA")
+            wrapper_count += 1
+            sample_count += 1
+            continue
+        else:       #tag mismatch
+            mismatch = MatchingMethods.get_tag_mismatch(MatchingMethods, sample_count, sample)
+            sample_count += mismatch.counter
+            generated_wrapper.append(mismatch.string)
+print(generated_wrapper)
